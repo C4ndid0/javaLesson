5ace836b5b6d4ce9e43eb7c2fb791a1b0387a9ad
C4ndid0 <avnsever@gmail.com>
2022-11-28 23:06:24 -0300

    variables-declaration

---
+++ aula09/Variables.java
@@ -0,0 +1,17 @@
+package aula09;
+
+public class Variables {
+  public static void main(String[] args){
+    int houseNumber;
+    String nameOf;
+
+    houseNumber = 22
+    ;
+    nameOf = "Bigulin";
+
+    System.out.println("house:" + houseNumber);
+    System.out.println("NameOf:" + nameOf);
+
+  }
+
+}
