52c6c30c3e8b67c2e84972ee9223d8d4d84ca3d4
C4ndid0 <avnsever@gmail.com>
2022-11-29 00:03:08 -0300

    PrimitiveTypes

---
+++ aula11/Float.java
@@ -0,0 +1,11 @@
+package aula11;
+
+public class Float {
+  public static void main(String[] args) {
+      String name = "tomato";
+      double price = 3.4;
+    
+
+    System.out.println("name:" + name + " price:" + price);
+  }  
+}
---
+++ aula11/PrimitiveTypes.java
@@ -0,0 +1,15 @@
+package aula11;
+
+public class PrimitiveTypes {
+  public static void main(String[] args) {
+    byte number01 = 8;
+    short number02 = 16;
+    int number04 = 32;
+    long number05 = 64;
+
+    System.out.println("number: " + number01);
+    System.out.println("number: " + number02);
+    System.out.println("number: " + number04);
+    System.out.println("number: " + number05);
+  }
+}
