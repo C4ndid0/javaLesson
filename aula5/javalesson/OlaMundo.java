594688cef0e3ef6bd5e1a8278fcc3632d68f9d39
C4ndid0 <avnsever@gmail.com>
2022-11-30 12:43:28 -0300

    clear

--- aula5/javalesson/OlaMundo.java
+++
@@ -1,7 +0,0 @@
-package aula5.javalesson;
-
-public class OlaMundo {
-  public static void main(String[] args){
-    System.out.println("Aoba Mundao");
-  }
-}
--- aula6/Argumentos.java
+++
@@ -1,7 +0,0 @@
-package aula6;
-
-public class Argumentos {
-  public static void main(String[] args){
-    System.out.println("Voce digitol: " + args[0]);
-  }
-}
