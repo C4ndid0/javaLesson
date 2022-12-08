1dd92ce765a603259a4d267bec33685e65620789
C4ndid0 <avnsever@gmail.com>
2022-12-08 03:58:03 -0300

    lamp

--- poo/Lampada.java
+++ poo/Lampada.java
@@ -1,12 +1,50 @@
 package poo;
 
 public class Lampada {
-  String marca;
-  String type;
-  double power;
-  double price;
+  private String modelo;
+  private String tensao;
+  private int potencia;
+  private double price;
+  private boolean ligada;
 
- void litesOn(){
-  System.out.println("Lites on");
- }
+	public String getTensao() {
+		return this.tensao;
+	}
+
+	public void setTensao(String tensao) {
+		this.tensao = tensao;
+	}
+
+	public int getPotencia() {
+		return this.potencia;
+	}
+
+	public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
+
+	public double getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public boolean isLigada() {
+		return this.ligada;
+	}
+
+	public void setLigada(boolean ligada) {
+		this.ligada = ligada;
+	}
+
+
+  public String getModelo(){
+    return modelo;
+  }
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+
 }
---
+++ poo/TesteLampada.java
@@ -0,0 +1,10 @@
+package poo;
+
+public class TesteLampada {
+  public static void main(String[] args) {
+    Lampada piscapisca = new Lampada();
+
+    piscapisca.setPrice(341);
+    System.out.print(piscapisca.getPrice());
+  }
+}
